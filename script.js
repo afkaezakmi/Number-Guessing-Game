dd5c759dd9c5909b21814c7711df26d983cc5bf9
kel <115203738+afkaezakmi@users.noreply.github.com>
2025-02-05 22:29:37 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const playBtn = document.querySelector('#playbtn');
 const contentContainer = document.querySelector('.content-container');
 const h1Text = document.querySelector('#h1Text');
+const centerContainer = document.querySelector('.center-container');
 
+//This function will be processed once the plyBtn is clicked
+//After pressed, it will remove the first contents inside the contentContainer and new elements will be presented
 const playBtnHandler = () => {
     playBtn.remove();
 
@@ -33,63 +36,188 @@ const playBtnHandler = () => {
         }
     });
 
-    submitButton.addEventListener('click', () => {
-       playBtn.removeEventListener('click', playBtnHandler);
-       userField.remove();
-       text1.remove();
-       submitButton.remove();
-        
-       const welcomeText = document.createElement('h3');
-       welcomeText.innerText = 'Hello, '+userField.value+'!';
-       contentContainer.append(welcomeText);
+    const submitEventRemover = () => {
+        playBtn.removeEventListener('click', playBtnHandler);
+        const username = userField.value;
+        submitBtnHandler(username);
+        submitButton.removeEventListener('click', submitEventRemover);
+    }
 
-       const h3Text = document.createElement('h5');
-       h3Text.innerText = 'SET THE MAX RANGE OF THE NUMBER YOU WILL GUESS!';
-       contentContainer.append(h3Text);
+    submitButton.addEventListener('click', submitEventRemover);
+};
 
-       const dropDown = document.createElement('label');
-       dropDown.setAttribute('for', 'selectContainer');
-       dropDown.innerText = 'Choose:';
-       
-       const select = document.createElement('select');
-       select.setAttribute('id', 'selectContainer');
-       select.classList.add('optionContainer');
-       let num = 50;
-       for(let i = 0; i <= 4; i++){
-            const option = document.createElement('option');
-            option.setAttribute('value', String(num));
-            option.innerText = String(num);
-            num += 50;
-            select.append(option);
-       }
-       contentContainer.append(dropDown);
-       contentContainer.append(select);
+const submitBtnHandler = (username) => {
+    contentContainer.innerHTML = '';
+    
+    const welcomeText = document.createElement('h3');
+    welcomeText.innerText = 'Hello, '+username+'!';
+    contentContainer.append(welcomeText);
 
-       const selectButton = document.createElement('button');
-       selectButton.innerText = 'Select';
-       selectButton.classList.add('submitButton');
-       contentContainer.append(selectButton);
+    const h3Text = document.createElement('h5');
+    h3Text.innerText = 'SET THE MAX RANGE OF THE NUMBER YOU WILL GUESS!';
+    contentContainer.append(h3Text);
 
-       selectButton.addEventListener('click', () => {
-            submitButton.removeEventListener('click', playBtnHandler);
-            h3Text.remove();
-            selectButton.remove();
-            dropDown.remove();
-            select.remove();
+    const dropDown = document.createElement('label');
+    dropDown.setAttribute('for', 'selectContainer');
+    dropDown.innerText = 'Choose:';
+    
+    const select = document.createElement('select');
+    select.setAttribute('id', 'selectContainer');
+    select.classList.add('optionContainer');
+    let num = 50;
+    for(let i = 0; i <= 4; i++){
+        const option = document.createElement('option');
+        option.setAttribute('value', String(num));
+        option.innerText = String(num);
+        num += 50;
+        select.append(option);
+    }
+    contentContainer.append(dropDown);
+    contentContainer.append(select);
 
-            const selectedValue = select.value;
+    const selectButton = document.createElement('button');
+    selectButton.innerText = 'Select';
+    selectButton.classList.add('submitButton');
+    contentContainer.append(selectButton);
 
-            const rangeText = document.createElement('h4');
-            rangeText.innerText = 'The Max Range of you will Guess is '+selectedValue+'!';
+    const selectEventRemover = () => {
+    selectButton.removeEventListener('click', selectEventRemover);
+    const selectedRange = select.value;
+    selectBtnHandler(username, selectedRange);
+    }
 
-            contentContainer.append(rangeText);
-       });
+    selectButton.addEventListener('click', selectEventRemover);
+};
 
+const selectBtnHandler = (username, selectedRange) => {
+    contentContainer.innerHTML = '';
+
+    const randomNumber = Math.floor(Math.random() * selectedRange) + 1;
+
+
+    const welcomeText = document.createElement('h3');
+    welcomeText.innerText = 'Hello, '+username+'!';
+    contentContainer.append(welcomeText);
+
+    const rangeText = document.createElement('h4');
+    rangeText.innerText = 'The Max Range of you will Guess is '+selectedRange+'!';
+    contentContainer.append(rangeText);
+
+    const guessField = document.createElement('input');
+    guessField.classList.add('guessField');
+    guessField.disabled = true;
+    contentContainer.append(guessField);
+
+    const resultText = document.createElement('h4');
+    resultText.innerText = 'First guess is Free!';
+    contentContainer.append(resultText);
+
+    const buttonDiv = document.createElement('div');
+    buttonDiv.classList.add('buttonDiv');
+    centerContainer.append(buttonDiv);
+
+    let btnValue = 9;
+    for(let i = btnValue; i >= 0; i--){
+        const buttons = document.createElement('button');
+        buttons.innerText = String(btnValue);
+        buttons.value = btnValue;
+        buttons.classList.add('digits')
+        buttonDiv.append(buttons);
+        btnValue -= 1;
+    }
+
+    const backspace = document.createElement('button');
+    backspace.innerText = '<-';
+    backspace.classList.add('backSpace');
+    backspace.style.backgroundColor = 'red';
+    buttonDiv.append(backspace);
+
+    const enterButton = document.createElement('button');
+    enterButton.innerText = 'Submit';
+    enterButton.style.backgroundColor = 'limegreen';
+    enterButton.classList.add('enter');
+    enterButton.disabled = true;
+    buttonDiv.append(enterButton);
+
+    const updateSubmitButton = () => {
+        if(guessField.value.length > 0){
+        enterButton.disabled = false;
+        } else {
+            enterButton.disabled = true;
+        }
+    };
+
+    const buttonDisabler = () => {
+        const numBtnDisabler = document.querySelectorAll('.digits');
+        if(guessField.value.length >= 3){
+            numBtnDisabler.forEach(buttons => {
+                buttons.disabled = true;       
+            });
+        } else {
+            numBtnDisabler.forEach(buttons => {
+                buttons.disabled = false;       
+            });
+        }
+    }
+
+    const numButton = document.querySelectorAll('.digits');
+    numButton.forEach(number => {
+        number.addEventListener('click', ()=> {
+            const value = number.getAttribute('value');
+            if(value){
+                guessField.value += value;
+                updateSubmitButton();
+                buttonDisabler();
+            }
+        });
     });
 
-}
+    backspace.addEventListener('click', () => {
+    guessField.value = guessField.value.slice(0, -1);
+    updateSubmitButton();
+    buttonDisabler();
+    });
+
+    guessField.addEventListener('input', updateSubmitButton);
+
+    enterButton.addEventListener('click', () => {
+        processGame(guessField.value, guessField, randomNumber, resultText);
+    });
+    
+};
 
 playBtn.addEventListener('click', playBtnHandler)
 
+const processGame = (guess, guessField, range, resultText) => {
 
+    let win;
 
+   if (guess == range){
+        resultText.innerText = 'YOUR GUESS IS CORRECT!';
+        win = true;
+        verdictContainer(range, win);
+   } else if (guess > range) {
+        win = false;
+        resultText.innerText = `${guess} IS HIGHER`;
+   } else {
+        win = false;
+        resultText.innerText = `${guess} IS LOWER!`;
+   }
+
+   guessField.value = '';
+}
+
+const verdictContainer = (rangeData, winOrLose) => {
+    const guessContainer = document.createElement('div');
+    guessContainer.classList.add('guessContainer');
+    const randomNumber = document.createElement('h5');
+    randomNumber.innerText = `THE NUMBER IS ${rangeData}!`;
+    guessContainer.append(randomNumber);
+    contentContainer.append(guessContainer);
+    
+    if (winOrLose === true){
+        guessContainer.style.backgroundColor = 'limegreen';
+    } else {
+        guessContainer.style.backgroundColor = 'red';
+    }
+}
